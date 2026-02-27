adaa2f12a763b462f0d5c2b13d4354e93c834ef6
Nguyen Van Giap <giapnv@gem-corp.global>
2026-02-27 10:37:35 +0700

    feat: Collection Framework excercise

---
+++ javadadvance/src/collectionwork/Ex1.java
@@ -0,0 +1,14 @@
+package collectionwork;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        List<String> list = new java.util.ArrayList<>(List.of("Nguyen Van A", "Tran Van B", "Nguyen Van C", "Hoang Van E"));
+        Collections.sort(list);
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
---
+++ javadadvance/src/collectionwork/Ex2.java
@@ -0,0 +1,29 @@
+package collectionwork;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        String content =
+                """
+                        The quick brown fox jumps over the lazy dog.
+                        This sentence contains every letter of the alphabet,
+                        making it perfect for typing practice.
+                        By typing this paragraph multiple times,
+                        you can improve your speed and accuracy.
+                        Focus on keeping a steady rhythm rather than just going as fast as you can.
+                        Consistent practice will surely lead to better typing skills over time
+                       """;
+        Map<String, Integer> map = new HashMap<>();
+        String[] tokens = content.split(" ");
+        for (String token : tokens) {
+            map.put(token, map.getOrDefault(token, 0) + 1);
+        }
+
+        System.out.println("Result");
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
