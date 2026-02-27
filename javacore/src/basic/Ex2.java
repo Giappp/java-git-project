07821b1899aef7bd31a7e7e335b43655b1cb8c11
Nguyen Van Giap <giapnv@gem-corp.global>
2026-02-27 13:34:09 +0700

    fix: handle runtime exception from java core exercise

--- javacore/src/basic/Ex2.java
+++ javacore/src/basic/Ex2.java
@@ -1,21 +1,15 @@
 package basic;
 
-import java.util.Scanner;
+import utils.InputUtility;
+
 /*
 Viết chương trình tính chu vi và diện tích hình tròn.
 */
 public class Ex2 {
     public static final double PI = 3.14;
 
-    public static final Scanner sc = new Scanner(System.in);
-
-    public static int getInteger(String message){
-        System.out.printf("%s",message);
-        return sc.nextInt();
-    }
-
     public static void main(String[] args) {
-        int r = getInteger("R = ");
+        float r = InputUtility.getFloat("R = ");
         System.out.println("Diameter: " + 2 * PI * r);
         System.out.println("Area: " + PI * r * r);
     }
--- javacore/src/oop3/App2.java
+++ javacore/src/oop3/App2.java
@@ -1,9 +1,6 @@
 package oop3;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.TreeMap;
 
 public class App2 {
     public static void main(String[] args) throws IOException {
